f92253484461720b5ce8a4f27bfe5ff8c1836307
tabishhameed <tabishhameed34@gmail.com>
2021-10-22 16:33:27 +0800

    whileloop

---
+++ whileloop.js
@@ -0,0 +1,15 @@
+var a = 0;
+
+while (a < 100) {
+  a += 1;
+  console.log(a);
+}
+
+
+
+function grreting () {
+  yes = prompt("whats you age");
+document.getElementById("fam").innerHTML = yes;
+
+}
+
